fb4cbf05edaeb868b736dab15b9d7e4ea6fa9f50
Kaushalya13 <nimanthikaushalya13@gmail.com>
2024-06-04 23:34:23 +0530

    Updating Employee.js

--- controller/Employee.js
+++ controller/Employee.js
@@ -11,7 +11,7 @@ function imageUploaded() {
 
 
 //Employee Save
-$("#btnSave").on('click', () => {
+$("#btnEmpSave").on('click', () => {
     if ($("#txtpassword").val()=== $("#rePassword").val()){
         let name = $("#empName").val();
         let pic = $("#pic").val();
@@ -317,6 +317,7 @@ const setValueEmployee = (response) => {
                           <td class='attached_branch'>${employee.attached_branch}</td>
                           <td class='address_line_01'>${employee.address_line_01} ${employee.address_line_02} ${employee.address_line_03} ${employee.address_line_04} ${employee.address_line_05} </td>
                           <td class='contact_no'>${employee.contact_no}</td>
+                          <td class='email'>${employee.email}</td>
                           <td class='informInCaseOfEmergency'>${employee.informInCaseOfEmergency}</td>
                           <td class='emergencyContact'>${employee.emergencyContact}</td>
                           <td>
@@ -327,11 +328,9 @@ const setValueEmployee = (response) => {
                           </td>
                         </tr>`;
         $("#employee-tbl").append(recode);
-
         $("#employee-tbl")
             .find("tr:last .btn-danger")
             .click(() => handleDeleteEmployeeOnClick(employee));
-
         $("#employee-tbl")
             .find("tr:last .btn-warning")
             .click(() => handleEditEmployeeOnClick(employee));
@@ -363,280 +362,20 @@ function viewImg(event) {
 }
 
 //Update
-$("#btnUpdate").prop("disabled", true);
+$("#btnEmpUpdate").prop("disabled", true);
 let selectedEmpId = null
+let selectedPic = null
 
-$("#btnUpdate").on("click", () => {
-    console.log("selected employee id", selectedEmpId)
-
-    if ($("#txtpassword").val()=== $("#rePassword").val()){
-        let name = $("#empName").val();
-        let pic = $("#pic").val();
-        let gender = $("select[name='gender']").val();
-        let status = $("select[name='status']").val();
-        let designation = $("input[name='designation']").val();
-        let role = $("select[name='role']").val();
-        let dob = $("#dob").val();
-        let joinDate = $("#joinDate").val();
-        let attachedBranch = $("input[name='attachedBranch']").val();
-        let address1 = $("input[name='address01']").val();
-        let address2 = $("input[name='address02']").val();
-        let address3 = $("input[name='address03']").val();
-        let address4 = $("input[name='address04']").val();
-        let address5 = $("input[name='address05']").val();
-        let contactNo = $("#empContactNo").val();
-        let email = $("#empEmail").val();
-        let informInCaseOfEmergency = $("#informInCaseOfEmergency").val();
-        let emergencyContact = $("input[name='emergencyContact']").val();
-        let password = $("#txtpassword").val();
-
-
-        if (!name) {
-            Swal.fire({
-                icon: "error",
-                title: "Please Check Name Field",
-                text: "Something went wrong!"
-            })
-            return
-        }
-
-        if (!pic) {
-            Swal.fire({
-                icon: "error",
-                title: "Please Check Img Field",
-                text: "Something went wrong!"
-            })
-            return
-        }
-        if (gender === "Select Gender") {
-            Swal.fire({
-                icon: "error",
-                title: "Please Check Gender Field",
-                text: "Something went wrong!"
-            })
-            return
-        }
-
-
-        if (status === "Status") {
-            Swal.fire({
-                icon: "error",
-                title: "Please Check Status Field",
-                text: "Something went wrong!"
-            })
-            return
-        }
-
-        if (!designation) {
-            Swal.fire({
-                icon: "error",
-                title: "Please Check Designation Field",
-                text: "Something went wrong!"
-            })
-            return
-        }
-
-        if (role === "Select Role") {
-            Swal.fire({
-                icon: "error",
-                title: "Please Check Role Field",
-                text: "Something went wrong!"
-            })
-            return
-        }
-
-        if (!dob) {
-            Swal.fire({
-                icon: "error",
-                title: "Please Check Date Of Birth Field",
-                text: "Something went wrong!"
-            })
-            return
-        }
-
-        if (!joinDate) {
-            Swal.fire({
-                icon: "error",
-                title: "Please Check Join Date Field",
-                text: "Something went wrong!"
-            })
-            return
-        }
-
-        if (!attachedBranch) {
-            Swal.fire({
-                icon: "error",
-                title: "Please Check Attached Branch Field",
-                text: "Something went wrong!"
-            })
-            return
-        }
-
-
-        if (!address1) {
-            Swal.fire({
-                icon: "error",
-                title: "Please Check Address 01 Field",
-                text: "Something went wrong!"
-            })
-            return
-        }
-
-        if (!address2) {
-            Swal.fire({
-                icon: "error",
-                title: "Please Check Address 02 Field",
-                text: "Something went wrong!"
-            })
-            return
-        }
-
-        if (!address3) {
-            Swal.fire({
-                icon: "error",
-                title: "Please Check Address 03 Field",
-                text: "Something went wrong!"
-            })
-            return
-        }
-
-        if (!address4) {
-            Swal.fire({
-                icon: "error",
-                title: "Please Check Address 04 Field",
-                text: "Something went wrong!"
-            })
-            return
-        }
-
-        if (!address5) {
-            Swal.fire({
-                icon: "error",
-                title: "Please Check Address 05 Field",
-                text: "Something went wrong!"
-            })
-            return
-        }
-
-        if (!contactNo) {
-            Swal.fire({
-                icon: "error",
-                title: "Please Check Contact No Field",
-                text: "Something went wrong!"
-            })
-            return
-        }
-
-        if (!email) {
-            Swal.fire({
-                icon: "error",
-                title: "Please Check Email Field",
-                text: "Something went wrong!"
-            })
-            return
-        }
-
-        if (!informInCaseOfEmergency) {
-            Swal.fire({
-                icon: "error",
-                title: "Please Check InformInCaseOfEmergency Field",
-                text: "Something went wrong!"
-            })
-            return
-        }
-
-        if (!emergencyContact) {
-            Swal.fire({
-                icon: "error",
-                title: "Please Check Emergency Contact Field",
-                text: "Something went wrong!"
-            })
-            return
-        }
-
-        if (!password) {
-            Swal.fire({
-                icon: "error",
-                title: "Please Check Password Field",
-                text: "Something went wrong!"
-            })
-            return
-        }
-
-        var formData = new FormData();
-
-        formData.append('emp_code',selectedEmpId);
-        formData.append('emp_name', name);
-        formData.append('emp_pro_pic', imageBase64);
-        formData.append('gender', gender);
-        formData.append('status', status);
-        formData.append('designation', designation);
-        formData.append('role', role);
-        formData.append('dob', dob);
-        formData.append('joinDate', joinDate);
-        formData.append('attached_branch', attachedBranch);
-        formData.append('address_line_01', address1);
-        formData.append('address_line_02', address2);
-        formData.append('address_line_03', address3);
-        formData.append('address_line_04', address4);
-        formData.append('address_line_05', address5);
-        formData.append('contact_no', contactNo);
-        formData.append('email', email);
-        formData.append('informInCaseOfEmergency', informInCaseOfEmergency);
-        formData.append('emergencyContact', emergencyContact);
-        formData.append('password', password);
-
-
-        $.ajax({
-            url: "http://localhost:9090/shop/api/v1/employee/update",
-            type: "PUT",
-            processData: false,
-            contentType: false,
-            data: formData,
-            headers: {
-                Authorization: "Bearer " + localStorage.getItem("token")
-            },
-
-            success: function (response) {
-                console.log("------------" + response)
-                if (response === false) {
-                    Swal.fire({
-                        icon: "error",
-                        title: "Oops...",
-                        text: "Email is already registered. Please change email."
-                    })
-                } else if (response === true) {
-                    Swal.fire({
-                        icon: "success",
-                        title: "Employee Updating Successful",
-                        showConfirmButton: false,
-                        timer: 1500
-                    })
-                    resetField()
-                    loadEmployeeData()
-                } else {
-                    Swal.fire({
-                        icon: "error",
-                        title: "Oops...",
-                        text: "An error occurred while saving employee data."
-                    })
-                }
-            },
-            error: function (xhr, status, error) {
-                console.error("Error:", xhr.responseText);
-            }
-        })
-        loadEmployeeData()
-    }
-})
 
 window.handleEditEmployeeOnClick = (employee) =>{
-    $("#btnSave").prop("disabled", false)
-    $("#btnUpdate").prop("disabled", true)
+    $("#btnEmpUpdate").prop("disabled", false)
+    $("#btnEmpSave").prop("disabled", true)
+
 
     selectedEmpId = employee.emp_code
+    selectedPic = employee.pic
+
     $("#empName").val(employee.emp_name)
-    $("#pic").val(employee.pic)
     $("select[name='gender']").val(employee.gender)
     $("select[name='status']").val(employee.status)
     $("input[name='designation']").val(employee.designation)
@@ -653,39 +392,139 @@ window.handleEditEmployeeOnClick = (employee) =>{
     $("#empEmail").val(employee.email)
     $("#informInCaseOfEmergency").val(employee.informInCaseOfEmergency)
     $("input[name='emergencyContact']").val(employee.emergencyContact)
-    $("#txtpassword").val(employee.password)
+    // $("#txtpassword").val(employee.password)
 
 }
 
+$("#btnEmpUpdate").on("click", () => {
+    console.log("selected employee id", selectedEmpId);
+
+    let name = $("#empName").val();
+    let gender = $("select[name='gender']").val();
+    let status = $("select[name='status']").val();
+    let designation = $("input[name='designation']").val();
+    let role = $("select[name='role']").val();
+    let dob = $("#dob").val();
+    let joinDate = $("#joinDate").val();
+    let attachedBranch = $("input[name='attachedBranch']").val();
+    let address1 = $("input[name='address01']").val();
+    let address2 = $("input[name='address02']").val();
+    let address3 = $("input[name='address03']").val();
+    let address4 = $("input[name='address04']").val();
+    let address5 = $("input[name='address05']").val();
+    let contactNo = $("#empContactNo").val();
+    let email = $("#empEmail").val();
+    let informInCaseOfEmergency = $("#informInCaseOfEmergency").val();
+    let emergencyContact = $("input[name='emergencyContact']").val();
+
+    if (!name || !gender || !status || !designation || !role || !dob || !joinDate || !attachedBranch || !address1 || !address2 || !address3 || !address4 || !address5 || !contactNo || !email || !informInCaseOfEmergency || !emergencyContact) {
+        Swal.fire({
+            icon: "error",
+            title: "Please fill all fields",
+            text: "Something went wrong!"
+        });
+        return;
+    }
+
+    if (gender === "Select Gender" || status === "Status" || role === "Select Role") {
+        Swal.fire({
+            icon: "error",
+            title: "Please select valid options",
+            text: "Something went wrong!"
+        });
+        return;
+    }
+
+    var formData = new FormData();
+    formData.append('emp_code', selectedEmpId);
+    formData.append('emp_name', name);
+    formData.append('emp_pro_pic', selectedPic);
+    formData.append('gender', gender);
+    formData.append('status', status);
+    formData.append('designation', designation);
+    formData.append('role', role);
+    formData.append('dob', dob);
+    formData.append('joinDate', joinDate);
+    formData.append('attached_branch', attachedBranch);
+    formData.append('address_line_01', address1);
+    formData.append('address_line_02', address2);
+    formData.append('address_line_03', address3);
+    formData.append('address_line_04', address4);
+    formData.append('address_line_05', address5);
+    formData.append('contact_no', contactNo);
+    formData.append('email', email);
+    formData.append('informInCaseOfEmergency', informInCaseOfEmergency);
+    formData.append('emergencyContact', emergencyContact);
+
+    $.ajax({
+        url: "http://localhost:9090/shop/api/v1/employee/update",
+        type: "PUT",
+        processData: false,
+        contentType: false,
+        data: formData,
+        headers: {
+            Authorization: "Bearer " + localStorage.getItem("token")
+        },
+        success: function (response) {
+            console.log("Response: ", response);
+            if (response === false) {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Email is already registered. Please change email."
+                });
+            } else if (response === true) {
+                Swal.fire({
+                    icon: "success",
+                    title: "Employee Updating Successful",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+                resetField();
+                loadEmployeeData();
+            } else {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "An error occurred while saving employee data."
+                });
+            }
+        },
+        error: function (xhr, status, error) {
+            console.error("Error:", xhr.responseText);
+        }
+    });
+});
 
 //Reset
-$("#reset").on("click", () => {
+$("#btnEmpReset").on("click", () => {
     resetField()
 })
-const resetField = () => {
-    $("#btnSave").prop("disabled", false)
-    $("#btnUpdate").prop("disabled", true)
+
+function resetField() {
+    $("#btnEmpSave").prop("disabled", false)
+    $("#btnEmpUpdate").prop("disabled", true)
     selectedEmpId = null
 
-    $("#empName").val("")
-    $("#pic").val("")
-    $("select[name='gender']").val("")
-    $("select[name='status']").val("")
-    $("input[name='designation']").val("")
-    $("select[name='role']").val("")
-    $("#dob").val("")
-    $("#joinDate").val("")
-    $("input[name='attachedBranch']").val("")
-    $("input[name='address01']").val("")
-    $("input[name='address02']").val("")
-    $("input[name='address03']").val("")
-    $("input[name='address04']").val("")
-    $("input[name='address05']").val("")
-    $("#empContactNo").val("")
-    $("#empEmail").val("")
-    $("#informInCaseOfEmergency").val("")
-    $("input[name='emergencyContact']").val("")
-    $("#txtpassword").val("")
+
+    $("#empName").val('');
+    $("#pic").val('');
+    $("select[name='gender']").val('Select Gender');
+    $("select[name='status']").val('Status');
+    $("input[name='designation']").val('');
+    $("select[name='role']").val('Select Role');
+    $("#dob").val('');
+    $("#joinDate").val('');
+    $("input[name='attachedBranch']").val('');
+    $("input[name='address01']").val('');
+    $("input[name='address02']").val('');
+    $("input[name='address03']").val('');
+    $("input[name='address04']").val('');
+    $("input[name='address05']").val('');
+    $("#empContactNo").val('');
+    $("#empEmail").val('');
+    $("#informInCaseOfEmergency").val('');
+    $("input[name='emergencyContact']").val('');
 }
 
 window.handleDeleteEmployeeOnClick = (employee) => {
